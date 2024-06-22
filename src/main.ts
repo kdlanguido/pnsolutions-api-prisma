0b995daa501c21ff9392a566983e9286c3d71082
kd <alyastubigman@gmail.com>
2024-06-22 18:49:01 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'https://pn-solutions.vercel.app/', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-});
-
   await app.listen(10000);
 }
 bootstrap();
